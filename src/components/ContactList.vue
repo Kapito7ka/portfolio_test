e0cef2594e9a9db705099c89e849a627dce5039d
Kapitoshka <vlada.sapitash@gmail.com>
2026-01-22 14:04:46 +0200

    create_Contact_components

---
+++ src/components/ContactList.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+  contacts: {
+    type: Object,
+    default: null
+  }
+})
+</script>
+
+<template>
+  <ul v-if="contacts">
+    <li>Телефон: {{ contacts.phone }}</li>
+    <li v-if="contacts.instagram">Instagram: <a :href="contacts.instagram" target="_blank">{{ contacts.instagram }}</a></li>
+    <li v-if="contacts.facebook">Facebook: <a :href="contacts.facebook" target="_blank">{{ contacts.facebook }}</a></li>
+    <li v-if="contacts.tiktok"> TikTok: <a :href="contacts.tiktok" target="_blank">{{ contacts.tiktok }}</a></li>
+  </ul>
+</template>
